7d27642f371ed34e9c58707a8a3b675674996921
erfaw <erfawn.h@gmail.com>
2025-11-28 18:53:55 +0330

    in 'imports.py': 1. add import from selenium (Chrome), 2. add header to apart sections

--- imports.py
+++ imports.py
@@ -1,11 +1,18 @@
+### 1. built-ins
 import datetime
 from datetime import timedelta
 import os.path
 import pytz
+
+### 2. third parties
+#google calendar api
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+#selenium
+from selenium.webdriver import Chrome
 
+### 3. self made
 from google_calendar_manager import GoogleCalendarManager
