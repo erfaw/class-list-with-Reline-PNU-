8569b5bfbf1dcce2d11990ac4bd23e5d206cbacb
erfaw <erfawn.h@gmail.com>
2025-11-28 20:19:42 +0330

    in 'main.py': 1. load_dotenv() added, 2. at section 'LOGGING IN TO 'RELINE_URL' logged in to reline successfully,

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from imports import *
 from consts import *
 import subprocess as sp; sp.call('cls', shell=True)
+load_dotenv()
 
-## MAKE CALENDAR OBJECT
+## MAKE CALENDAR and CHROME DRIVER OBJECT
+chrome = ChromeManager()
 calendar = GoogleCalendarManager()
 
+## LOGGING IN TO 'RELINE_URL'
+chrome.get_reline()
+chrome.login_reline(username=os.environ.get('USERNAME_RELINE'), password= os.environ.get("PASSWORD_RELINE"))
 
 
 ## make a test event to learn.
