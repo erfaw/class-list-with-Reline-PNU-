df5e8d8169c37883333655bb83d012a6b1d60041
erfaw <erfawn.h@gmail.com>
2025-11-28 18:10:12 +0330

    add EVENT_COLOR dictionary

--- consts.py
+++ consts.py
@@ -1,1 +1,15 @@
 SCOPES = ["https://www.googleapis.com/auth/calendar.events"]
+
+EVENT_COLOR = {
+    "lavender": 1,
+    "sage": 2,
+    "grape": 3,
+    "flamingo": 4,
+    "banana": 5,
+    "tangerine": 6,
+    "peacock": 7,
+    "graphite": 8,
+    "blueberry": 9,
+    "basil": 10,
+    "tomato": 11,
+}
