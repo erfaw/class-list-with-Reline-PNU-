bbc32f5f1fd3ba32c1b949a89dc908aa534ae333
erfaw <erfawn.h@gmail.com>
2025-11-28 14:24:44 +0330

    moved all imports to 'imports.py', 2. in 'quickstart.py' remove and replace imports with 'from imports import *', 3. in 'google_calendar_manager.py' add 'import' statement, initialized for build a class to manage process with 'Google Calendar API',

--- quickstart.py
+++ quickstart.py
@@ -1,12 +1,5 @@
 ### FULLY COPY FROM GOOGLE API DOCS QUICK START
-import datetime
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+from imports import *
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/calendar.events"]
