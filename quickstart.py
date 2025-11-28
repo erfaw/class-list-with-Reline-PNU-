4c5f0c51148b95e25c3fd114ff489334729f476b
erfaw <erfawn.h@gmail.com>
2025-11-28 15:06:06 +0330

    in 'quickstart.py': 1. remove codes and move to 'google_calendar_manager.py' in class 'GoogleCalendarManager()', 2. replace code with these changes, 3. add comments for explanation, run-check: good

--- quickstart.py
+++ quickstart.py
@@ -1,58 +1,21 @@
 ### FULLY COPY FROM GOOGLE API DOCS QUICK START
 from imports import *
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/calendar.events"]
-
+from consts import *
 
 def main():
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
+    ## MAKE CALENDAR OBJECT AND CHECK FOR TOKEN CREDENTIALS
+    calendar = GoogleCalendarManager()
+    calendar.check_token()
 
     try:
-        service = build("calendar", "v3", credentials=creds)
+        ## MAKE CALENDAR SERVICE, TO DO API CALLS THROUGH IT
+        calendar.service = calendar.make_service_for_calendar()
 
         # Call the Calendar API
-        now = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
-        print("Getting the upcoming 10 events")
-        events_result = (
-            service.events()
-            .list(
-                calendarId="primary",
-                timeMin=now,
-                maxResults=10,
-                singleEvents=True,
-                orderBy="startTime",
-            )
-            .execute()
-        )
-        events = events_result.get("items", [])
-
-        if not events:
-            print("No upcoming events found.")
-            return
-
-        # Prints the start and name of the next 10 events
-        for event in events:
-            start = event["start"].get("dateTime", event["start"].get("date"))
-            print(start, event["summary"])
+        calendar.get_and_print_upcoming_10_events()
 
     except HttpError as error:
         print(f"An error occurred: {error}")
