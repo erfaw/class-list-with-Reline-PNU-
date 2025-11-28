91a2a4a4d5c7fc85a4790cdc5174063d8a6bedf2
erfaw <erfawn.h@gmail.com>
2025-11-28 18:39:30 +0330

    in 'quickstart.py': 1. add 'cls' subprocess to clear console to first of project, 2. change 'Call the Calendar API' section to 'Call FOR NEXT 10 EVENTS', 3. done TODO: 'make a test event to learn.', 4. add 1 TODO, 5. use 'calendar.make_new_event()' to make a test event

--- quickstart.py
+++ quickstart.py
@@ -1,6 +1,7 @@
 ### FULLY COPY FROM GOOGLE API DOCS QUICK START
 from imports import *
 from consts import *
+import subprocess as sp; sp.call('cls', shell=True)
 
 def main():
     """Shows basic usage of the Google Calendar API.
@@ -10,14 +11,25 @@ def main():
     calendar = GoogleCalendarManager()
     calendar.check_token()
 
+
     try:
         ## MAKE CALENDAR SERVICE, TO DO API CALLS THROUGH IT
         calendar.service = calendar.make_service_for_calendar()
 
-        # Call the Calendar API
+        # Call FOR NEXT 10 EVENTS
         # calendar.get_and_print_upcoming_10_events()
-
-        #TODO: make a test event to learn.
+        
+        ## make a test event to learn.
+        calendar.make_new_event(
+            # TODO: check if this code need change or not (based on input from reline)
+            start_time= datetime.time(7,0),
+            end_time=datetime.time(8,0),
+            title='salam',
+            # start_date=datetime.date(year=2025,month=10,day=27),
+            # end_date=datetime.date(year=2025,month=10,day=27),
+            
+        )
+        input()
 
     except HttpError as error:
         print(f"An error occurred: {error}")
