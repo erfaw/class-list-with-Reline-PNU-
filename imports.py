5996f7603a7e7761001d6ccdffd408f7a000759e
erfaw <erfawn.h@gmail.com>
2025-11-30 14:06:47 +0330

    in 'imports.py': 1. add a inner function : 'exit_on_dependency_error()' to print a message and exit program if it called, 2. pytz moved to section '2. third parties', 3. pathlib Path moved to section '1. built-ins', 4. in section '2. third parties': all of imports rewrite with try-except statement for exception ImportError and used 'exit_on_dependency_error()' on that exception (which kills program) ,

--- imports.py
+++ imports.py
@@ -1,36 +1,76 @@
+def exit_on_dependency_error(package_name, install_command):
+    """Prints a clear error message and exits the program."""
+    print("\n" + "="*70)
+    print(f"FATAL ERROR: Required dependency '{package_name}' is not installed.")
+    print("Please install it using the following command:")
+    print(f"\n   >>> {install_command}")
+    print("="*70 + "\n")
+    sys.exit(1)
+
 ### 1. built-ins
 import datetime
 from datetime import timedelta
 import os.path
-import pytz
 import time
 import json
 import requests
+import sys
+from pathlib import Path
 
 ### 2. third parties
 #google calendar api
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+try:
+    from google.auth.transport.requests import Request
+    from google.oauth2.credentials import Credentials
+    from google_auth_oauthlib.flow import InstalledAppFlow
+    from googleapiclient.discovery import build
+    from googleapiclient.errors import HttpError
+except ImportError:
+    exit_on_dependency_error("Google API Libraries", "pip install google-api-python-client google-auth-httplib2 google-auth-oauthlib")
+
 #selenium
-from selenium.webdriver import Chrome
-from selenium.webdriver import ChromeOptions
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+try:
+    from selenium.webdriver import Chrome
+    from selenium.webdriver import ChromeOptions
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support import expected_conditions as EC
+    from selenium.webdriver.support.ui import WebDriverWait
+except ImportError:
+    exit_on_dependency_error("Selenium", "pip install selenium")
+
 #dotenv
-from dotenv import load_dotenv
+try:
+    from dotenv import load_dotenv
+except ImportError:
+    exit_on_dependency_error("python-dotenv", "pip install python-dotenv")
+
 #pandas
-import pandas as pd
-from pathlib import Path
+try:
+    import pandas as pd
+except ImportError:
+    exit_on_dependency_error("pandas", "pip install pandas")
+
 #jdatetime
-import jdatetime
-#plyer
-from plyer import notification
+try:
+    import jdatetime
+except ImportError:
+    exit_on_dependency_error("jdatetime", "pip install jdatetime")
+
+# pytz (Needed for timezone awareness, even though it's often a dependency of others)
+try:
+    import pytz
+except ImportError:
+    exit_on_dependency_error("pytz", "pip install pytz")
+
+# plyer
+try:
+    from plyer import notification
+except ImportError:
+    exit_on_dependency_error("plyer", "pip install plyer")
 
 ### 3. self made
 from google_calendar_manager import GoogleCalendarManager
 from chrome_manager import ChromeManager
 from consts import *
+
+## TODO: GOOD TO BE KIND OF CODE HERE: TO CHECK WITH ALL 'requirements.txt': if user had it: skip, if user didn't had it: pip install for it
